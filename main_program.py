5402576e9ce2990232b565238ed156ee59934d79
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-03-24 13:27:33 +0200

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -1,33 +1,29 @@
 import MyModule
 
-# Lists to store the usernames and passwords
-usernames = []
-passwords = []
-
-# Main program loop
 while True:
-    print("\n1. Register")
-    print("2. Login")
-    print("3. Change Name or Password")
-    print("4. Recover Forgotten Password")
-    print("5. Exit")
+    print("\nVALIK:")
+    print("1. Registreerimine")
+    print("2. Sisse logimine")
+    print("3. Parooli muutmine")
+    print("4. Parooli taastamine")
+    print("5. Välja logimine")
+    print("6. Lõpp")
     
-    choice = input("Choose an option (1-5): ")
+    if MyModule.sees:
+        print(f"Tere, {MyModule.user}!")
     
-    if choice == '1':
-        # Register a new user
-        MyModule.register_user(usernames, passwords)
-    elif choice == '2':
-        # User login
-        MyModule.login_user(usernames, passwords)
-    elif choice == '3':
-        # Change username or password
-        MyModule.change_credentials(usernames, passwords)
-    elif choice == '4':
-        # Recover forgotten password
-        MyModule.recover_password(usernames, passwords)
-    elif choice == '5':
-        print("Goodbye!")
-        break  # Exit the loop and end the program
+    v=input("Valik: ")
+    
+    if v=="1":
+        MyModule.reg()
+    elif v=="2":
+        MyModule.login()
+    elif v=="3":
+        MyModule.parool()
+    elif v=="4":
+        MyModule.taasta()
+    elif v=="5":
+        MyModule.logout()
+        break
     else:
-        print("Invalid choice, please try again.")
+        print("Vale valik")
